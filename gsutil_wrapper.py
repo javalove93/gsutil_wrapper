d4bbcbf6e116b642a7ebce549f6233f20a900b2d
Jerry Jeong <javalove93@gmail.com>
2020-06-22 23:13:41 +0900

    options

--- gsutil_wrapper.py
+++ gsutil_wrapper.py
@@ -1,10 +1,17 @@
 import argparse
 import subprocess
 
+opt_MAX_PROCESSES = 20
+opt_MAX_TEMP_STORAGE = 10
+
 def run_prog_get_output(prog):
 	p = subprocess.Popen(prog, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	return p.stdout.readlines()
 
+def run_gsutil(list):
+	for entry in list:
+		size, source, dest = entry
+		print(size, source, dest)
 
 
 if __name__ == '__main__':
@@ -19,20 +26,27 @@ if __name__ == '__main__':
 	s_list_all = []
 
 	for line in path_list:
-		entry = line.rstrip().split()
-		opt = None
-		if len(entry) == 3:
-			source, dest, opt = entry
+		if line.startswith('#'):
+			opt, value = line[1:].strip().split()
+			if opt == 'MAX_TEMP_STORAGE':
+				opt_MAX_TEMP_STORAGE = int(value)
+			elif opt == 'MAX_PROCESSES':
+				opt_MAX_PROCESSES = int(value)
 		else:
-			source, dest = entry
-		print(source)
-		print(dest)
-		print(opt)
+			entry = line.rstrip().split()
+			opt = None
+			if len(entry) == 3:
+				source, dest, opt = entry
+			else:
+				source, dest = entry
+			print(source)
+			print(dest)
+			print(opt)
 
-		s_list = run_prog_get_output("gsutil du " + source)
-		
-		for s in s_list:
-			s_list_all.append(s.rstrip().split() + [dest])
+			s_list = run_prog_get_output("gsutil du " + source)
+			
+			for s in s_list:
+				s_list_all.append(s.rstrip().split() + [dest])
 	
-	print(s_list_all)
+	run_gsutil(s_list_all)
 
