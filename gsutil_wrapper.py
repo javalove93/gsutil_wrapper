ec2cc5f37ef5085734e211e69b7f21f73ab7ccea
Jerry Jeong <javalove93@gmail.com>
2020-06-22 21:34:17 +0900

    run aws s3

--- gsutil_wrapper.py
+++ gsutil_wrapper.py
@@ -1,8 +1,11 @@
 import argparse
-
+import subprocess
 
 def run_prog_get_output(prog):
 	print(prog)
+	p = subprocess.Popen(prog, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	for line in p.stdout.readlines():
+		print(line)
 	return "hello"
 
 
@@ -17,6 +20,16 @@ if __name__ == '__main__':
 	path_list = f_path_list.readlines()
 
 	for line in path_list:
-		result = run_prog_get_output(line)
+		entry = line.rstrip().split()
+		opt = None
+		if len(entry) == 3:
+			source, dest, opt = entry
+		else:
+			source, dest = entry
+		print(source)
+		print(dest)
+		print(opt)
+		result = run_prog_get_output("aws s3 ls " + source)
+		
 		print(result)
 
