b9b0480127a3749f3c14cb5e11e6411f2dfda93a
Jerry Jeong <javalove93@gmail.com>
2020-06-22 21:22:06 +0900

    pathlist

--- gsutil_wrapper.py
+++ gsutil_wrapper.py
@@ -1,8 +1,22 @@
 import argparse
 
-parser = argparse.ArgumentParser(prog='gsutil_wrapper', description='gsutil wrapper')
-parser.add_argument('source_file')
-parser.add_argument('dest_file')
 
-args = parser.parse_args()
+def run_prog_get_output(prog):
+	print(prog)
+	return "hello"
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(prog='gsutil_wrapper', description='gsutil wrapper')
+	parser.add_argument('path_list')
+
+	args = parser.parse_args()
+
+	f_path_list = open(args.path_list)
+	path_list = f_path_list.readlines()
+
+	for line in path_list:
+		result = run_prog_get_output(line)
+		print(result)
 
